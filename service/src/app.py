78ae7d126ed665693468cfde32b5c7eb8a38ca88
AlexandruDumitriu <a.dumitriu-1@student.tudelft.nl>
2024-05-12 19:12:46 +0200

    Create the 2 endpoints on the backend

--- service/src/app.py
+++ service/src/app.py
@@ -1,10 +1,55 @@
-from flask import Flask
+"""
+Flask application for version retrieval and prediction.
+
+This module contains a Flask application that provides two endpoints:
+- /API/v1.0/version: Retrieves the version of the application.
+- /API/v1.0/predict: Accepts input data and returns a prediction.
+
+It uses the VersionUtil class from lib_version_group3 to retrieve the version,
+and sends a POST request to a model service for prediction.
+
+"""
+
+import os
+from flask import Flask, jsonify, request
+from lib_version_group3.version import VersionUtil
+from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-if __name__=='__main__':
-  app.run(host='0.0.0.0', port=106, debug=True)
+@app.route('/API/v1.0/version')
+def version():
+    """
+    Endpoint to retrieve the version of the application.
+
+    Returns:
+        JSON: A JSON object containing the version information.
+    """
+    version_string = VersionUtil.get_version()
+    version_data = {'version': version_string}
+    return jsonify(version_data)
+
+
+@app.route('/API/v1.0/predict', methods=['POST'])
+def predict():
+    """
+    Endpoint to predict based on input data.
+
+    Returns:
+        JSON: A JSON object containing the prediction result.
+    """
+    url = request.json.get('inputPrediction')
+    response = requests.post(
+        os.environ.get('MODEL_SERVICE') + '/API/v1.0/other_endpoint',
+        json={"url": url},
+        timeout=10
+    )
+    prediction = response.json()
+    return jsonify(prediction)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=106, debug=True)
